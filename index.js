440795d420276860abd7414405abaaf8bfe18a85
janaugstein <jan_augstein@yahoo.de>
2023-03-11 20:25:16 +0100

    added cors policy

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 const sessionModel = require("./models/sessionModel");
 const app = express();
+const cors = require("cors");
 
 require("dotenv").config({ path: "./config.env" });
 const port = 8001;
 
 const connectionString = process.env.DBCONNECTION;
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    //credentials: true
+  })
+);
+
 mongoose.connect(connectionString).then(() => {
   app.get("/", (req, res) => {
     res.send("<p>Hello World</p>");
   });
   //when joining a session: check req.sessionID and send back the maps
-  app.get("/joinSession", (req, res) => {
+  app.post("/joinSession", (req, res) => {
     console.log(req);
     res.send("You joined Session XY");
   });
