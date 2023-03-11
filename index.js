f8732d887e184cfd7e4be9ba6d21390471267e65
janaugstein <jan_augstein@yahoo.de>
2023-03-11 19:29:38 +0100

    added DB connection

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const sessionModel = require("./models/sessionModel");
 const app = express();
-const port = 3000;
+const port = 8001;
 
-app.get("/", (req, res) => [res.send("<p>Hello World</p>")]);
+const connectionString =
+  "mongodb+srv://janA:jan97315@cluster0.bm2xy.mongodb.net/csmapvoting?retryWrites=true&w=majority";
 
-app.listen(port, () => [console.log(`Example app listening on port ${port}`)]);
+mongoose.connect(connectionString).then(() => {
+  app.get("/", (req, res) => [res.send("<p>Hello World</p>")]);
+  app.listen(port, () => [
+    console.log(`Example app listening on port ${port}`),
+  ]);
+  const newSession = new sessionModel({
+    sessionID: 1,
+    map1: "cache",
+    map2: "inferno",
+    map3: "anubis",
+    votesMap1: 2,
+    votesMap2: 1,
+    votesMap3: 0,
+    whoVoted: [543, 215],
+  });
+  //newSession.save();
+});
---
+++ models/sessionModel.js
@@ -0,0 +1,21 @@
+//Require Mongoose
+const mongoose = require("mongoose");
+
+// Define schema
+const Schema = mongoose.Schema;
+
+const sessionSchema = new Schema({
+  sessionID: Number,
+  map1: String,
+  map2: String,
+  map3: String,
+  votesMap1: Number,
+  votesMap2: Number,
+  votesMap3: Number,
+  whoVoted: [Number],
+});
+
+// Compile model from schema
+const Session = mongoose.model("Session", sessionSchema);
+
+module.exports = Session;
